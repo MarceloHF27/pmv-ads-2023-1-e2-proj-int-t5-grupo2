f7ecad1c08195cdbe9f68d622ba3e5aed342f185
Remilton <111924209+remiltonjr@users.noreply.github.com>
2023-06-03 20:18:31 -0300

    Classes Adicionadas

---
+++ SOS_Buscas_V2/Models/ContatoModel.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SOS_Buscas_V2.Models
+{
+    public class ContatoModel
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Column(TypeName = "varchar(20)")]
+        public string Celular { get; set; }
+
+        [Column(TypeName = "varchar(60)")]
+        public string EmailContato { get; set; }
+
+        [ForeignKey("Email")]
+        [Column(TypeName = "varchar(45)")]
+        public string EmailUsuario { get; set; }
+
+        public UsuarioModel Email { get; set; }
+    }
+}
+
